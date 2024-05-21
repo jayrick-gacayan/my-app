fd52d8fd8a13b9ce23654eb9bf2119b7bfb72145
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-05-21 17:51:44 +0800

    feat: create custom table component

--- app/sections/table.tsx
+++ app/sections/table.tsx
@@ -1,47 +1,77 @@
-import { ReactNode } from "react"
+import { ReactNode, useCallback } from "react"
 
-export interface ColumnDef<T> { 
-  key: keyof T,
+type ColumnDefProps = {
   title: string;
-  render?: (column: ColumnDef<T>, item: T) => ReactNode;
-};
+  sortable?: boolean;
+}
 
-interface DataProps<T>{
+export type ColumnDef<T> = ({
+  key: string,
+  renderCell: (item: T) => ReactNode;
+} & ColumnDefProps) |
+  ({
+    key: keyof T,
+    renderCell?: ((item: T, column: ColumnDef<T>) => ReactNode);
+  } & ColumnDefProps);
+
+interface DataProps<T> {
   data: T[];
   columns: ColumnDef<T>[];
 }
 
+function isDataColumn<T>(column: ColumnDef<T>): column is ColumnDef<T> & { key: keyof T } {
+  return !(column.key in {} as T);
+}
+
 export default function Table<T>({
   data,
-  columns 
-}: DataProps<T>) { 
+  columns
+}: DataProps<T>) {
+
+
+
   return (
     <table className="w-full">
       <thead>
-        {
-          columns.map((column, columnIndex) => {
-            return (
-              <th key={`table-header-cell-${columnIndex}`}
-                className="bg-blue-600 text-white p-2">
-                {column.title}
-              </th>
-            )
-          })
-        }
+        <tr>
+          {
+            columns.map((column, columnIndex) => {
+              return (
+                <th key={`table-header-cell-${columnIndex}`}
+                  className="bg-blue-600 text-white p-2">
+                  {column.title}
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                    width="0.63em" height="1em" viewBox="0 0 320 512">
+                    <path fill="currentColor"
+                      d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41m255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41" />
+                  </svg>
+                </th>
+              )
+            })
+          }
+        </tr>
       </thead>
       <tbody>
         {
-          data.map((dataValue, dataValueIndex: number) => { 
+          data.map((dataValue, dataValueIndex: number) => {
             return (
-              <tr key={`table-body-row-${dataValueIndex}`}>
+              <tr key={`table-body-row-${dataValueIndex}-${JSON.stringify(dataValue)}`}>
                 {
-                  columns.map((columnValue, columnValueIndex: number) => { 
+                  columns.map((columnValue, columnValueIndex) => {
+                    if (isDataColumn(columnValue)) {
+                      return (
+                        <td key={`table-row-cell-${columnValueIndex}`}>
+                          {
+                            !columnValue.renderCell ?
+                              `${dataValue[columnValue.key]}` :
+                              columnValue.renderCell(dataValue, columnValue)
+                          }
+                        </td>
+                      )
+                    }
                     return (
-                      <td key={`table-row-cell-${columnValueIndex}`}>
-                        {
-                          columnValue.render ? 
-                            columnValue.render(columnValue, dataValue) : `${dataValue[columnValue.key]}`
-                        }
+                      <td key={`table-row-cell-${columnValueIndex}-${JSON.stringify(dataValue)}`}>
+                        {columnValue.renderCell(dataValue)}
                       </td>
                     )
                   })
---
+++ app/table-view/page.tsx
@@ -0,0 +1,72 @@
+import Table, { ColumnDef } from "../sections/table";
+import Image from "next/image";
+export interface Sample {
+  id: number;
+  name: string;
+  description: string;
+  imageURL: string;
+}
+
+const data: Sample[] = [
+  {
+    id: 1,
+    name: 'sample',
+    imageURL: 'https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'sample',
+  },
+  {
+    id: 2,
+    name: 'sample',
+    description: 'sample',
+    imageURL: 'https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 3,
+    name: 'sample',
+    description: 'sample',
+    imageURL: 'https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+]
+
+const columns: ColumnDef<Sample>[] = [
+  {
+    key: 'id',
+    title: 'ID',
+    sortable: true,
+  },
+  {
+    key: 'name',
+    title: 'Name',
+    sortable: true,
+  },
+  {
+    key: 'description',
+    title: 'Description',
+    sortable: true,
+  },
+  {
+    key: 'imageURL',
+    title: 'Photo',
+    renderCell: (item, column) => {
+      return <Image src={item.imageURL} width={50} height={50} alt={`${item.id}-${item.name}`} />
+    },
+  },
+  {
+    key: 'action',
+    title: 'Actions',
+    renderCell: (item) => { 
+      return (
+        <div>
+          This is the action container.
+        </div>
+      )
+    }
+  },
+  
+]
+
+export default function Page() {
+  return (
+    <Table columns={columns} data={data} />
+  )
+}
